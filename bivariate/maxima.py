f6e2425f1b7970fb94c72ed4da13fbc8dfa5c1cf
Aleboul <82594658+Aleboul@users.noreply.github.com>
2021-07-27 17:45:56 +0200

    Add files via upload

---
+++ bivariate/maxima.py
@@ -0,0 +1,95 @@
+"""
+    Produit l'expérience lorsque l'on prend le maximum de variables aléatoires suivant
+    une gaussienne bivariée (liée à la Hüsler Reiss)
+"""
+import numpy as np
+import pandas as pd
+import monte_carlo
+import extreme_value_copula
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+plt.style.use('seaborn-whitegrid')
+from matplotlib import cm
+n = 100
+#n_iter = 1000
+#n_sample = [256]
+#n_max = [64,128,256]
+theta = 0.2
+#
+#class MaxBivGau(object):
+#    """
+#        Base class for sample from the maximum of the bivariate gaussian
+#
+#        Parameters
+#        ----------
+#            theta (float or None) : Parameter for the copula.
+#            n_max ([int]) : Number of data where we took the maximum.
+#            n_sample ([int]) : lengths of the sample.
+#        Attributes
+#        ----------
+#        sample (2-D array like, of shape (n_sample,2))
+#        
+#    """
+#
+#    def __init__(self, random_seed = None, theta = None, n_sample = None, n_max = None):
+#
+#        self.random_seed = random_seed
+#        self.theta = theta
+#        self.n_max = n_max
+#        self.n_sample = n_sample
+#        self.mean = [5,5]
+#        self.cov = [[1, 1 - theta / np.log(self.n_max)], [1 - theta / np.log(self.n_max), 1]]
+#
+#    def sample(self, inv_cdf = None):
+#        sample = np.zeros((self.n_sample,2))
+#        for n in range(0, self.n_sample):
+#            sample_ = np.random.multivariate_normal(self.mean, self.cov, size = self.n_max)
+#            max_ = [np.max(sample_[:,0]), np.max(sample_[:,1])]
+#            sample[n] = max_
+#
+#        return(sample)
+#
+#x = np.linspace(0.01,0.99,n)
+#var_lmbd = pd.DataFrame()
+#for n in n_max:
+#    value = []
+#    var_lmbd_ = pd.DataFrame()
+#    copula = MaxBivGau(theta = theta, n_sample= np.max(n_sample), n_max = n)
+#    Monte = monte_carlo.Monte_Carlo(n_iter= n_iter, n_sample= n_sample, copula= copula)
+#    var = Monte.exec_varlmbd(inv_cdf= None,lmbds= x)
+#    var_lmbd_['var_emp'] = var
+#    copula = extreme_value_copula.Hussler_Reiss(theta = theta)
+#    for lmbd in x:
+#        value_ = copula.var_FMado(lmbd)
+#        value.append(value_)
+#    var_lmbd_['var_theo'] = value
+#    var_lmbd_['lmbd'] = x
+#    var_lmbd_['n_max'] = n
+#    var_lmbd  = var_lmbd.append(var_lmbd_)
+#
+#print(var_lmbd)
+#
+
+with open("output/max_student_M100_n128.txt", "rb") as data:
+    b = pickle.load(data)
+
+var_lmbd = pd.DataFrame(b)
+x = np.linspace(0.01,0.99,n)
+value = []
+copula = extreme_value_copula.Student(theta = theta, psi1=3.0)
+for lmbd in x:
+    value_ = copula.var_FMado(lmbd)
+    value.append(value_)
+
+var_lmbd['var_theo'] = value
+
+print(var_lmbd)
+
+fig, ax = plt.subplots()
+sns.scatterplot(data = var_lmbd, x = "lambda", y = "var_emp", hue = "nmax", palette= 'crest', s = 10, alpha = 0.5)
+sns.lineplot(data = var_lmbd, x = "lambda", y = "var_theo", hue = "nmax", palette = 'crest',legend = False, lw = 1)
+ax.legend(title = r'$\theta$')
+ax.set_ylabel(r'$\sigma^2$')
+ax.set_xlabel(r'$\lambda$')
+plt.savefig("/home/aboulin/Documents/stage/var_FMado/bivariate/output/image_4.pdf")
---
+++ bivariate/maximal.R
@@ -0,0 +1,88 @@
+library(VineCopula)
+library(doRNG)
+library(dplyr)
+
+prefix = "/home/aboulin/Documents/stage/var_FMado/bivariate/output/"
+
+target <- list()
+
+fmado_  <- function(xvec, yvec, lambda){
+    F_X = ecdf(xvec)(xvec)
+    G_Y = ecdf(yvec)(yvec)
+
+    value_ = 0.5 * mean(abs(F_X^lambda - G_Y^(1-lambda)))
+    return(value_)
+}
+
+target$generate_randomness <- function(nobservation){
+    sample_ = BiCopSim(nobservation * nmax, 2, 0.8, 3)
+    return(sample_)
+}
+
+target$robservation <- function(randomness){
+    sample = apply(matrix(t(randomness), ncol = nmax),1, max)
+    return(matrix(sample, ncol = 2, byrow = T))
+}
+
+M = 100 # number of iteration
+n = c(256) # length of sample
+nmax_ = c(16,32,64,128,256,512)
+
+filename <- paste0(prefix, "max_student_M", M, "_n", nmax_[6], ".txt")
+
+simu = function(target){
+	foreach(rep = 1:M, .combine = rbind) %dorng% {
+		# foreach is a function that create a loop and we, at each iteration, we increment the matrix of results (here output)
+		# using rbind.
+		# Allocate space for output
+
+		FMado_store = matrix(0, length(n))
+		FMado_runtimes = rep(0, length(n))
+		FMado_lambda = rep(0, length(n))
+
+		# generate all observations and sets of randomness to be used
+		obs_rand = target$generate_randomness(max(n)) # we produce our bivariate vector of data
+		obs_all = target$robservation(obs_rand) # some data are now hidden
+
+		for(i in 1:length(n)){
+			t_FMado = proc.time() # we compute the time to estimate
+			# subset observations
+			obs = obs_all[1:n[i],] # we pick the n[i] first rows, i.e 50 rows for the first, 100 for the second
+			### We compute the lambda FMadogram
+			FMado = fmado_(obs[,1], obs[,2], lambda) # we compute now the lambda-FMadogram (the normalized one)
+			t_FMado = proc.time() - t_FMado
+
+			# Save the results
+			FMado_store[i,] = FMado
+			FMado_runtimes[i] = t_FMado[3]
+			FMado_lambda[i] = lambda
+        }
+
+		output = cbind(FMado_store, FMado_runtimes,FMado_lambda, n,nmax)
+		output
+	}
+}
+
+lambda_ = seq(0.01, 0.99, length.out = 100)
+store_ = matrix(ncol = 6, nrow = 0)
+for (i in 1:length(nmax_)){
+	nmax = nmax_[i]
+	print(i)
+	lambda_FMadogram = foreach(rep = 1:length(lambda_), .combine = rbind) %dorng% {
+			lambda = lambda_[rep]
+			prod = simu(target)
+			scaled = (prod[,1]) * sqrt(prod[,4])
+			output = cbind(prod, scaled)
+	}
+	store_ = rbind(store_,lambda_FMadogram)
+	
+}
+print(store_)
+df_FMado = data.frame(store_)
+names(df_FMado) = c("FMado", "runtime", "lambda", "n", "nmax", "scaled")
+var = df_FMado %>% group_by(lambda, nmax) %>% summarise(var_emp = var(scaled))
+print(var)
+require('reticulate')
+
+py_save_object(var, filename)
+
