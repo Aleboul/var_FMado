330d9eaeecfc72ca40463d0914348fc2bccf8a59
Alexis Boulin <82594658+Aleboul@users.noreply.github.com>
2021-08-27 13:51:50 +0200

    Add files via upload

--- missing_data/monte_carlo.py
+++ missing_data/monte_carlo.py
@@ -71,7 +71,7 @@ class Monte_Carlo(object):
 			ecdf[i] = (1.0 / np.sum(miss)) * np.sum((data <= data[i]) * miss)
 		return ecdf
 		
-	def _dist(self, X, miss):
+	def _dist(self, X, miss, corr):
 		"""	
 			Compute matrix of F-Madogram using the empirical cumulative distribution function
 			
@@ -79,6 +79,8 @@ class Monte_Carlo(object):
 			------
 			   X : a matrix composed of uniform ecdf
 			lmbd : a parameter between 0 and 1
+			miss : array indicates missing
+			corr : if true, return corrected version
 			
 			Outputs
 			-------
@@ -87,21 +89,15 @@ class Monte_Carlo(object):
 		
 		ncols = X.shape[1]
 		nrows = X.shape[0]
-		dist = np.zeros([nrows, nrows]) # initialization
-		for i in range(0, nrows):
-			for j in range(0, i):
-				if i == j:
-					dist[i,i] = 0
-				else :
-					F_x = np.squeeze(X[j,:])
-					G_y = np.squeeze(X[i,:])
-					d = np.linalg.norm((np.power(F_x,self.lmbd) - np.power(G_y,1-self.lmbd)) * (miss[0] * miss[1]), ord = 1) - self.lmbd * np.sum((1-np.power(F_x,self.lmbd))* (miss[0] * miss[1])) - (1-self.lmbd) * np.sum((1-np.power(G_y,1-self.lmbd))* (miss[0] * miss[1]))
-					dist[i,j] = d
-					dist[j,i] = d
-
+		F_x = np.squeeze(X[0,:])
+		G_y = np.squeeze(X[1,:])
+		if corr :
+			dist = np.linalg.norm((np.power(F_x,self.lmbd) - np.power(G_y,1-self.lmbd)) * (miss[0] * miss[1]), ord = 1) - self.lmbd * np.sum((1-np.power(F_x,self.lmbd))* (miss[0] * miss[1])) - (1-self.lmbd) * np.sum((1-np.power(G_y,1-self.lmbd))* (miss[0] * miss[1]))
+		else :
+			dist = np.linalg.norm((np.power(F_x,self.lmbd) - np.power(G_y,1-self.lmbd)) * (miss[0] * miss[1]), ord = 1)
 		return dist
 		
-	def _fmado(self, X, miss) :
+	def _fmado(self, X, miss, corr) :
 		"""
 			This function computes the lambda-FMadogram
 			
@@ -110,6 +106,7 @@ class Monte_Carlo(object):
 			   X : a matrix
 			lmbd : constant between 0 and 1
 			miss : list of array of indicator
+			corr : If true, return corrected version of lambda-FMadogramme
 			Outputs
 			-------
 			matrix equals to 0 if i = j and equals to |F(X)^{\lmbd} - G(Y)^{1-\lmbd}|
@@ -123,11 +120,14 @@ class Monte_Carlo(object):
 			X_vec = np.array(X[:,p])
 			Femp = self._ecdf(X_vec, miss[p])
 			V[:,p] = Femp
-		FMado = self._dist(np.transpose(V), miss) / (2 * np.sum(miss[0] * miss[1])) + 0.5 * (1-self.lmbd + np.power(self.lmbd,2)) / ((1+self.lmbd) / (1+1-self.lmbd))
-		
+		if corr :
+			FMado = self._dist(np.transpose(V), miss, corr) / (2 * np.sum(miss[0] * miss[1])) + 0.5 * (1-self.lmbd + np.power(self.lmbd,2)) / ((1+self.lmbd) / (1+1-self.lmbd))
+		else :
+			FMado = self._dist(np.transpose(V), miss, corr) / (2 * np.sum(miss[0] * miss[1]))
+
 		return FMado
 		
-	def simu(self, inv_cdf):
+	def simu(self, inv_cdf, corr = {False, True, "Both"}):
 		"""
 			Perform Monte Carlo simulation
 		"""
@@ -136,34 +136,67 @@ class Monte_Carlo(object):
 		
 		for k in range(self.n_iter):
 			FMado_store = np.zeros(len(self.n_sample))
+			if corr == "Both":
+				FMado_corr_store = np.zeros(len(self.n_sample))
 			obs_all = self.copula.sample(inv_cdf)
 			miss_all = [np.random.binomial(1,self.p[0],np.max(self.n_sample)),np.random.binomial(1,self.p[1],self.n_sample)]
 			for i in range(0,len(self.n_sample)):
 				obs = obs_all[:self.n_sample[i]]
 				miss = [miss_all[0][:self.n_sample[i]],miss_all[1][:self.n_sample[i]]]
-				FMado = self._fmado(obs, miss)
-				FMado_store[i] = FMado[0,1]
-				
-			output_cbind = np.c_[FMado_store, self.n_sample, np.arange(len(self.n_sample))]
+				if corr == "Both":
+					FMado = [self._fmado(obs, miss, corr = True), self._fmado(obs, miss, corr = False)]
+					FMado_store[i], FMado_corr_store[i] = FMado[0], FMado[1]
+				else :
+					FMado = self._fmado(obs, miss, corr)
+					FMado_store[i] = FMado
+
+			if corr == "Both":
+				output_cbind = np.c_[FMado_store, FMado_corr_store, self.n_sample, np.arange(len(self.n_sample))]
+			else:
+				output_cbind = np.c_[FMado_store, self.n_sample, np.arange(len(self.n_sample))]
 			output.append(output_cbind)
 		df_FMado = pd.DataFrame(np.concatenate(output))
-		df_FMado.columns = ['FMado', 'n', 'gp']
-		df_FMado['scaled'] = (df_FMado.FMado - self.copula.true_FMado(self.lmbd)) * np.sqrt(df_FMado.n)
+		if corr == "Both":
+			df_FMado.columns = ['FMado', 'FMado_corr', 'n', 'gp']
+			df_FMado['scaled'] = (df_FMado.FMado - self.copula.true_FMado(self.lmbd)) * np.sqrt(df_FMado.n)
+			df_FMado['scaled_corr'] = (df_FMado.FMado_corr - self.copula.true_FMado(self.lmbd)) * np.sqrt(df_FMado.n)
+		else:
+			df_FMado.columns = ['FMado', 'n', 'gp']
+			df_FMado['scaled'] = (df_FMado.FMado - self.copula.true_FMado(self.lmbd)) * np.sqrt(df_FMado.n)
 		
 		return(df_FMado)
 		
 		
-	def exec_varlmbd(self, lmbds, inv_cdf, n_lmbds = 50):
+	def exec_varlmbd(self, lmbds, inv_cdf, n_lmbds = 50, corr = True):
+		"""
+			Performs Monte Carlo simulation in order to compute the empirical variance for
+			a given grid of lambda
+
+			Inputs
+			------
+			lmbds : array given the grid of lambda
+			n_lmbds : if a grid is not given, split the interval [0,1] in a chain with n_lmbds number
+			corr : version of the estimator.
+
+			Return
+			------
+			array of variance for a given lambda.
+		"""
 		if lmbds is None :
-			lmbds = np.linspace(0,1, n_lmbds)
+			lmbds = np.linspace(0.01,0.99, n_lmbds)
 		else :
 			lmbds = lmbds
 		for i, n in enumerate(self.n_sample):
 			var_lmbd = []
 			for lmbd in tqdm(lmbds):
 				self.lmbd = lmbd
-				output = self.simu(inv_cdf)
-				output = output['scaled'].var()
+				if corr == "Both":
+					output = self.simu(inv_cdf, corr)
+					output = [output['scaled'].var(), output['scaled_corr'].var()]
+				else :
+					output = self.simu(inv_cdf)
+					output = output['scaled'].var()
+
 				var_lmbd.append(output)
 				
 		return var_lmbd
