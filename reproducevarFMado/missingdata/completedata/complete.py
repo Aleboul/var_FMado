8a263293e7036ed1e598420d473f5f9be1e6bada
Alexis Boulin <82594658+Aleboul@users.noreply.github.com>
2021-09-27 17:07:31 +0200

    Update complete.py

--- reproducevarFMado/missingdata/completedata/complete.py
+++ reproducevarFMado/missingdata/completedata/complete.py
@@ -12,9 +12,9 @@
     p : array of missing's probabilities
 """
 
-import archimedean
-import extreme_value_copula
-import monte_carlo
+import var_FMado.reproducevarFMado.missingdata.src.archimedean
+import var_FMado.reproducevarFMado.missingdata.src.extreme_value_copula
+import var_FMado.reproducevarFMado.missingdata.src.monte_carlo
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
